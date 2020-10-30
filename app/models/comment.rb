c392e3fc052c4c5112bebc6f98b391c4515cd939
Newland-A <ajn252@gmail.com>
2020-10-30 07:54:19 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  # Nested Attributes is a feature that allows you to save attributes of a record through its associated parent.
+  accepts_nested_attributes_for :user
 
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?    
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,20 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  #comes from activerecord for nested attributes for model
+  accepts_nested_attributes_for :categories
 
-
+#creates a custom setter for the category attributes
+  def categories_attributes=(category_attributes)
+    #goes through each of the categories values
+    category_attributes.values.each do |category_attribute|
+      # if the attribute is present 
+      if category_attribute["name"].present?
+        # it will find or create
+        category = Category.find_or_create_by(category_attribute)
+        # then pushes the varibale category into post categories
+        self.categories << category
+      end
+    end
+  end
 end
